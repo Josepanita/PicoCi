5014ec893ae76c75e0b6a8afa565790a28f7d6c7
Jose Gomez <jose.panita@hotmail.com>
2012-07-24 17:38:02 -0430

    start develop

---
+++ tests/typecast.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+entero a;
+a = 42;
+printf("%d\n", a);
+
+real b = 64;
+printf("%.2f\n", b);
+
+entero c = 12, d = (entero) 'A';
+printf("%d, %d\n", c, d);
+
+void main() {
+}
